9e9fc64df7b6e7f6cb9d1a802547b5f269d19e4a
Shambhavi Singh <77614217+sshambhavii@users.noreply.github.com>
2021-06-19 02:00:36 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,55 @@
+let allMusic = [{
+        name: "Fix You",
+        artist: "Coldplay",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Do Me a Favour",
+        artist: "Arctic Monkeys",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Payphone",
+        artist: "Maroon 5",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Counting Stars",
+        artist: "OneRepublic",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Kill for You",
+        artist: "Skylar Grey feat.Eminem",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Chasing Cars",
+        artist: "Snow Patrol",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Catastrophize ",
+        artist: "Noah Kahan",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Not Afraid",
+        artist: "Eminem",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Arcade",
+        artist: "Duncan Laurence",
+        img: "music-9",
+        src: "music-9"
+    },
+];
